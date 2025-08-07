603c6af1911c8b04e37596ca15f97aaa5aa8733c
Deju Adejo <ojomideju2003@gmail.com>
2025-08-07 11:06:34 +0100

    edited footer postion: fixed

--- client/src/layout/Footer.tsx
+++ client/src/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-full">
+    <div className="fixed bottom-0 p-6 flex flex-col justify-center items-center w-full">
       <div>Footer Component</div>
     </div>
   );
